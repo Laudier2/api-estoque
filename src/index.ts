87addca4748d1f190a464baff3e2645e74b2381b
Laudier2 <trabalhandooline2019@hotmail.com>
2023-01-29 02:06:24 -0300

    Nova update

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,15 @@
 import express from "express";
-import { config } from "dotenv"
+//import { config } from "dotenv"
 import cors from "cors"
 import { router } from "./routes/routes";
 
-const app = express();
+const port = process.env.PORT || 3000
 
-const port = process.env.APP_PORT || 5000
+const app = express();
 
 app.use(express.json());
 app.use(cors());
 app.use(router);
-app.use(config);
+//app.use(config);
 
 app.listen(port, () => console.log(`Server na URL ${port}`));
